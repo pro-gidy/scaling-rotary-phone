7723597aa802bc8954153f649d39f6f0a693128b
pro-gidy <thompsonnoel610@gmail.com>
2026-02-15 17:03:33 +0000

    Add files via upload

---
+++ input.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+int main () {
+
+    // cout << (insertion operator)
+    // cin << (extraction)
+
+    string name;
+    int age;
+    cout << "Enter name: ";
+    cin >> name;
+
+    cout << "What is your age ?: ";
+    std::cin >> age;
+
+    cout << "Hello " << name << endl;
+    cout << "You are " << age  << "years old" << endl;
+
+
+
+
+
+    return 0;
+}
+// Created by user on 14/02/2026.
+//
---
+++ main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+int main() {
+
+    //type conversion
+    cout << (char) 100 << endl;
+    int correct = 8;
+    int questions = 10;
+    double score = correct/(double)questions * 100  ;
+    cout << score << endl;
+
+
+
+
+
+
+
+    return 0;
+}
