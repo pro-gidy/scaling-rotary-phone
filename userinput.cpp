41eb12316bc4067ed90126492d51a97e14e9355e
pro-gidy <thompsonnoel610@gmail.com>
2026-02-16 00:08:01 +0000

    Add files via upload

---
+++ userinput.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+
+int main() {
+    int x = 5;
+    int y = 6;
+    int sum = x + y;
+
+    std::cout << x << '\n';
+    std::cout << y << '\n';
+    std::cout <<sum<< '\n';
+
+
+    // integer (whole number)
+    int year = 2026;
+    int days = 122;
+    int age = 12;
+
+    // double (decimal numbers)
+    double price = 6.50;
+    double gpa = 2.5;
+
+    // char (single character)
+    char grade = 'A';
+    char initial = 'B';
+
+
+    //boolean (True or False)
+    bool student = true;
+    bool forSale = false;
+
+    //string (objects that repersent a sequencr of texts)
+
+    std::string name = "Bro" ;
+    std::cout <<name ;
+    std::cout << "Hello. " << name;
+
+
+    return 0;
+}
